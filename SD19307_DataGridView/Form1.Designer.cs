bd8a8ed706410fc759b378c753d25800ac27d9e8
charleshuynguyen <huynq.charles@gmail.com>
2024-03-13 13:24:57 +0700

    first commit

---
+++ SD19307_DataGridView/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace SD19307_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDSSinhVien = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMonHoc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rdoNam = new System.Windows.Forms.RadioButton();
+            this.rdoNu = new System.Windows.Forms.RadioButton();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSSinhVien)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDSSinhVien
+            // 
+            this.dgvDSSinhVien.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDSSinhVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDSSinhVien.Location = new System.Drawing.Point(40, 465);
+            this.dgvDSSinhVien.Name = "dgvDSSinhVien";
+            this.dgvDSSinhVien.RowHeadersWidth = 82;
+            this.dgvDSSinhVien.RowTemplate.Height = 33;
+            this.dgvDSSinhVien.Size = new System.Drawing.Size(1021, 652);
+            this.dgvDSSinhVien.TabIndex = 0;
+            this.dgvDSSinhVien.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDSSinhVien_CellClick);
+            this.dgvDSSinhVien.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDSSinhVien_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnSua);
+            this.panel1.Controls.Add(this.btnThem);
+            this.panel1.Controls.Add(this.rdoNu);
+            this.panel1.Controls.Add(this.rdoNam);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtMonHoc);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtHoTen);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dgvDSSinhVien);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(61, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1101, 1179);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(240, 25);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(529, 50);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(240, 122);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(529, 50);
+            this.txtHoTen.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 44);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ho Ten";
+            // 
+            // txtMonHoc
+            // 
+            this.txtMonHoc.Location = new System.Drawing.Point(240, 229);
+            this.txtMonHoc.Name = "txtMonHoc";
+            this.txtMonHoc.Size = new System.Drawing.Size(529, 50);
+            this.txtMonHoc.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 44);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mon Hoc";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 343);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 44);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Gioi Tinh";
+            // 
+            // rdoNam
+            // 
+            this.rdoNam.AutoSize = true;
+            this.rdoNam.Location = new System.Drawing.Point(297, 340);
+            this.rdoNam.Name = "rdoNam";
+            this.rdoNam.Size = new System.Drawing.Size(131, 48);
+            this.rdoNam.TabIndex = 8;
+            this.rdoNam.TabStop = true;
+            this.rdoNam.Text = "Nam";
+            this.rdoNam.UseVisualStyleBackColor = true;
+            // 
+            // rdoNu
+            // 
+            this.rdoNu.AutoSize = true;
+            this.rdoNu.Location = new System.Drawing.Point(553, 344);
+            this.rdoNu.Name = "rdoNu";
+            this.rdoNu.Size = new System.Drawing.Size(98, 48);
+            this.rdoNu.TabIndex = 9;
+            this.rdoNu.TabStop = true;
+            this.rdoNu.Text = "Nu";
+            this.rdoNu.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(833, 25);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(228, 50);
+            this.btnThem.TabIndex = 10;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(833, 118);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(228, 50);
+            this.btnSua.TabIndex = 11;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(833, 229);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(228, 50);
+            this.btnXoa.TabIndex = 12;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 1273);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSSinhVien)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDSSinhVien;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.RadioButton rdoNu;
+        private System.Windows.Forms.RadioButton rdoNam;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMonHoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
---
+++ SD19307_DataGridView/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SD19307_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        private int indexHienTai = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // them cac cot cho DataGridView
+            //dgvDSSinhVien.Columns.Add("ID", "ID");
+            //dgvDSSinhVien.Columns.Add("HoTen", "Ho Ten");
+            //dgvDSSinhVien.Columns.Add("MonHoc", "Mon Hoc");
+            //dgvDSSinhVien.Columns.Add("GioiTinh", "Gioi Tinh");
+
+            // them cac cot cho DataTable dt
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("HoTen", typeof(string));
+            dt.Columns.Add("MonHoc", typeof(string));
+            dt.Columns.Add("GioiTinh", typeof(bool));
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            // lay thong tin tu form 
+            int id = Convert.ToInt32(txtID.Text);
+            string hoTen = txtHoTen.Text;
+            string monHoc = txtMonHoc.Text;
+            bool gioiTinh = true;
+            if (rdoNam.Checked == true)
+            {
+                gioiTinh = true;
+            }
+            if (rdoNu.Checked == true)
+            {
+                gioiTinh = false;
+            }
+
+            // co 2 cach de them 1 row vao DataGridView
+            // cach 1
+            //dgvDSSinhVien.Rows.Add(id, hoTen, monHoc, gioiTinh);
+
+            // cach 2
+            //DataGridViewRow dr = new DataGridViewRow();
+            //dr.CreateCells(dgvDSSinhVien);
+            //dr.Cells[0].Value = id;
+            //dr.Cells[1].Value = hoTen;
+            //dr.Cells[2].Value = monHoc;
+            //dr.Cells[3].Value = gioiTinh;
+
+            //dgvDSSinhVien.Rows.Add(dr);
+
+            // them 1 row vao DataTable dt
+            DataRow dataRow = dt.NewRow();
+            dataRow["ID"] = id;
+            dataRow["HoTen"] = hoTen;
+            dataRow["MonHoc"] = monHoc;
+            dataRow["GioiTinh"] = gioiTinh;
+            dt.Rows.Add(dataRow);
+
+            dgvDSSinhVien.DataSource = dt;
+        }
+
+        private void dgvDSSinhVien_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dgvDSSinhVien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow selectedRow = dgvDSSinhVien.Rows[e.RowIndex];
+            txtID.Text = selectedRow.Cells[0].Value.ToString();
+            txtHoTen.Text = selectedRow.Cells[1].Value.ToString();
+            txtMonHoc.Text = selectedRow.Cells[2].Value.ToString();
+            if (selectedRow.Cells[3].Value.ToString() == "True")
+            {
+                rdoNam.Checked = true;
+            }
+            if (selectedRow.Cells[3].Value.ToString() == "False")
+            {
+                rdoNu.Checked = true;
+            }
+
+            indexHienTai = e.RowIndex;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            DataRow rowHientai = dt.Rows[indexHienTai];
+            rowHientai["ID"] = Convert.ToInt32(txtID.Text);
+            rowHientai["HoTen"] = txtHoTen.Text;
+            rowHientai["MonHoc"] = txtMonHoc.Text;
+            if (rdoNam.Checked == true)
+            {
+                rowHientai["GioiTinh"] = true;
+            }
+            if (rdoNu.Checked == true)
+            {
+                rowHientai["GioiTinh"] = false;
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            dt.Rows.RemoveAt(indexHienTai);
+        }
+    }
+}
